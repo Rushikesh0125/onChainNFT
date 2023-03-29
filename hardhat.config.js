db6c1c316625a2f22f31a887dc45b811d7e63fef
Rushikesh Jadhav <hellorushihere@gmail.com>
2023-03-29 12:54:01 +0530

    Added verification and quicknode provider

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     mumbai: {
-      url: process.env.TESTNET_RPC,
+      url: process.env.QUICKNODE_HTTP_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
